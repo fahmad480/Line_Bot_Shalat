5920d6d674a441cca9bfd9110d4db6474e1c10ce
zFz0000 <33687170+zFz0000@users.noreply.github.com>
2017-12-17 06:15:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -43,7 +43,7 @@ function shalat($keyword) {
     $response = Unirest\Request::get("$uri");
 
     $json = json_decode($response->raw_body, true);
-    $result = "Halo " .$profil->displayName.", Jadwal Shalat Hari ini Di Sekitar";
+    $result = "Jadwal Shalat Sekitar ";
 	$result .= $json['location']['address'];
 	$result .= "\nTanggal : ";
 	$result .= $json['time']['date'];
