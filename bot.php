a0d01524276ad81efb1ac0433f1af574a56ca3cc
zFz0000 <33687170+zFz0000@users.noreply.github.com>
2017-12-17 06:27:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,8 +9,8 @@ Modified @ Farzain - zFz
 require_once('./line_class.php');
 require_once('./unirest-php-master/src/Unirest.php');
 
-$channelAccessToken = 'KSqC4L4DQB5o2uk3eZTIwSNQgUGKoMF451X9VIkgmlzzDTEw+yCoA1eknDJ8HQM/IK0aLhJYABpYBZZpInVA7tksnVvLen2MUIDGwR8MtG1DBnz9LFcip99gZJ0zqCaezrR3tQlGlK8Xhb7Gz3nPMAdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-$channelSecret = 'd735bbd21936b39f05224829eaed6f50';//sesuaikan
+$channelAccessToken = '<Channel-Access-Token>'; //sesuaikan 
+$channelSecret = '<Channel-Secret>';//sesuaikan
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
