75227457031b66f2d275be09092c99f0aef51be5
zxzcafzvsx <31308588+zxzcafzvsx@users.noreply.github.com>
2017-12-17 05:39:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,24 +14,79 @@ $channelSecret = 'd735bbd21936b39f05224829eaed6f50';//sesuaikan
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
 $userId 	= $client->parseEvents()[0]['source']['userId'];
+$groupId 	= $client->parseEvents()[0]['source']['groupId'];
 $replyToken = $client->parseEvents()[0]['replyToken'];
 $timestamp	= $client->parseEvents()[0]['timestamp'];
-
+$type 		= $client->parseEvents()[0]['type'];
 
 $message 	= $client->parseEvents()[0]['message'];
 $messageid 	= $client->parseEvents()[0]['message']['id'];
 
 $profil = $client->profil($userId);
 
-$pesan_datang = $message['text'];
+$pesan_datang = explode(" ", $message['text']);
+
+$command = $pesan_datang[0];
+$options = $pesan_datang[1];
+if (count($pesan_datang) > 2) {
+    for ($i = 2; $i < count($pesan_datang); $i++) {
+        $options .= '+';
+        $options .= $pesan_datang[$i];
+    }
+}
+
+#-------------------------[Function]-------------------------#
+function urb_dict($keyword) {
+    $uri = "http://api.urbandictionary.com/v0/define?term=" . $keyword;
+
+    $response = Unirest\Request::get("$uri");
 
 
+    $json = json_decode($response->raw_body, true);
+    $result = $json['list'][0]['definition'];
+    $result .= "\n\nExamples : \n";
+    $result .= $json['list'][0]['example'];
+    return $result;
+}
+#-------------------------[Function]-------------------------#
+
+# require_once('./src/function/search-1.php');
+# require_once('./src/function/download.php');
+# require_once('./src/function/random.php');
+# require_once('./src/function/search-2.php');
+# require_once('./src/function/hard.php');
+
+//show menu, saat join dan command /menu
+if ($type == 'join' || $command == '/menu') {
+    $text = "Kenapa gua diundang ke grup lu? ketik /keyword kalau mau tahu gua berguna gk buat nie grup\n\nKlo lu gk suka ama gua, usir aja gua dari sini, ketik \keluar";
+    $balas = array(
+        'replyToken' => $replyToken,
+        'messages' => array(
+            array(
+                'type' => 'text',
+                'text' => $text
+            )
+        )
+    );
+}
 
 //pesan bergambar
-if($message['type']=='text')
-{
-	if($pesan_datang=='1')
-	{
+if($message['type']=='text') {
+	    if ($command == 'def') {
+
+
+        $balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+                array(
+                    'type' => 'text',
+                    'text' => 'Definition : ' . urb_dict($options)
+                )
+            )
+        );
+    }
+	else
+	if($pesan_datang=='1') {
 		
 		
 		$balas = array(
